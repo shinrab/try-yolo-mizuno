2a90f96e93c867c7613edc536bf4c30869793e29
mizzzuno <mizz22@outlook.jp>
2026-04-20 14:35:38 +0900

    add res-example

---
+++ res-example.py
@@ -0,0 +1,13 @@
+{
+  "person_id": 1,
+  "attributes": {
+    "hat": True,
+    "glasses": True,
+    "long_sleeve": True,
+    "trousers": True,
+    "handbag": False,
+    "shoulderbag": False,
+    "backpack": True
+  },
+  "timestamp": "2026-04-14 15:20:00"
+}
